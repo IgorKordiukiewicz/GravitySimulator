b5c48e121c5c389c0ef5ebf8a8e6945fba771b6e
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2021-04-30 08:42:06 +0200

    Removed unit tests project

---
+++ UnitTests/ArrowShapeTest.cpp
@@ -0,0 +1,13 @@
+#include "CppUnitTest.h"
+#include "../RocketSimulator/Include/ArrowShape.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(ArrowShapeTest)
+	{
+	public:
+		
+	};
+}
